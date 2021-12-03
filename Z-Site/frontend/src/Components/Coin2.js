18f0aecc3e3e9493c618269f8789d14ce234c5b7
Simeon <ssimeonovts@gmail.com>
2021-12-03 14:02:35 +0200

    fixed time and render

--- Z-Site/frontend/src/Components/Coin2.js
+++ Z-Site/frontend/src/Components/Coin2.js
@@ -4,38 +4,10 @@ import './Coin2.css'
 
 let timing = 100;
 
-function tomorrowCoinS(coins,today,currentTime){
-    let tmrArr = [];
-    let todaY = today
-    let currentTimE = currentTime
-    let coinS = coins
-    console.log('fun',coinS.length)
-    let coinsLenght = coinS.length
-    for(let i =0;i<coinsLenght;i++){
-        if(coinS[i].Date + 0.1 == todaY){
-            if(coinS[i].Time > currentTimE){
-                if(coinS[i].DateFormated.length > 20){
-                   //2021-11-22T22:14:00.000Z
-                   let indexT = coinS[i].DateFormated.indexOf('T')
-                   let indexD = coinS[i].DateFormated.indexOf('.')
-                   
-                   let newDate = coinS[i].DateFormated.slice(indexT+1,indexD-3)
-                   coinS[i].DateFormated = newDate
-                }
-                if(coinS[i].Poo != null){
-                    coinS[i].Poo2 = coinS[i].Poo.slice(0,32)
-                }
-               
-                tmrArr.push(coinS[i])
-            }
-        }
-    }
-    return tmrArr 
-}
-
 function Coin2({props}){
     const [coins,setCoins] = useState([])
     const [tomorrowCoins,setTomorrowCoins] = useState([])
+    const [tomorrowHelper,setTomorrowHelper] = useState([])
     const [todayDisplay,setTodayDisplay] = useState(",")
     const [tomorrowDisplay,setTomorrowDisplay] = useState(",")
     const [counter,setCounter] = useState(0)
@@ -47,18 +19,29 @@ function Coin2({props}){
                 //вземаме сегашното време UTC в дата и час - в числа -----
                 //тук е голям мармалад има по-добра имплентация ,но за сега това върши работа ;_;
                 let date = new Date()
-                let year = date.getUTCFullYear()
-                let month = date.getUTCMonth() + 1;
+                // let year = date.getUTCFullYear()
+                // let month = date.getUTCMonth() + 1;
+                // let daysInMonth = new Date(year,month,0).getDate();
+                // let day = date.getUTCDate();
+                // let hour = date.getUTCHours();
+                // let minutes = date.getUTCMinutes();
+
+                let year = date.getFullYear()
+                let month = date.getMonth() + 1;
                 let daysInMonth = new Date(year,month,0).getDate();
-                let day = date.getUTCDate();
-                let hour = date.getUTCHours();
-                let minutes = date.getUTCMinutes();
+                let day = date.getDate();
+                let hour = date.getHours();
+                let minutes = date.getMinutes();
+                
                 if(minutes < 10){
                     minutes = '0' + minutes
                 }
                 if(day < 10){
                     day = "0" + day
                 }
+                let utcDiff = -(new Date().getTimezoneOffset() / 60)
+                
+
                 let currentTime = hour + '.' + minutes 
                 let todayD = month  + '/' + day + '/' + year;
                 setTodayDisplay(todayD)
@@ -67,6 +50,8 @@ function Coin2({props}){
                 let helperMonth = Number(month)
                 let helperYear = year
                 
+
+                // утрешната дата ако мине дните на месеца да смени месеца или година и
                 if(helperDay +1 > daysInMonth){
                     helperMonth += 1;
                     helperDay = 1;
@@ -78,7 +63,7 @@ function Coin2({props}){
                         helperDay = "0" + helperDay
                     }
                     tomorrowD = helperMonth + '/' + helperDay +"/" + helperYear;
-                    console.log('ttt',tomorrowD)
+                   
                 }else {
                     helperDay += 1
                     if(helperDay <10){
@@ -88,12 +73,16 @@ function Coin2({props}){
                 }
 
                 let tomorrowCheck = helperMonth + "."+helperDay
+                let daysInMonthCoin = helperMonth +"."+daysInMonth
+                daysInMonthCoin = Number(daysInMonthCoin)
+                
                 tomorrowCheck = Number(tomorrowCheck)
-
+                
                 setTomorrowDisplay(tomorrowD)
                 let today = month +'.' + day
                 currentTime = Number(currentTime)
                 today = Number(today)
+               
                 //------------------------------------------------------------
 
                 //филтриране на токените -------------------------------------
@@ -101,6 +90,7 @@ function Coin2({props}){
                 let coinsLenght = coinS.length
                 let filteredCoins = [];
                 let tmrCoins = [];
+                let tmrHelper = [];
 
                     for(let i =0;i<coinsLenght;i++){
                         if(coinS[i].Date !=null){
@@ -112,39 +102,143 @@ function Coin2({props}){
                             if(typeof coinS[i].Time == 'string'){
                                 coinS[i].Time = Number(coinS[i].Time.replace(':',"."))        
                             }
-                        } 
+                        }
+                        coinS[i].Time2 = coinS[i].Time + utcDiff 
+
+
+                        
+                        if(coinS[i].Time2 >= 24){
+                            
+                            let helperDate = coinS[i].Date + 0.01
+                            
+                            if(helperDate > daysInMonthCoin){
+                                helperDate = month +1+ helperDate - daysInMonthCoin
+                                
+                                if(helperDate > 12.31){
+                                    helperDate = 1.01
+                                }
+                            }
+                            coinS[i].Date = helperDate 
+                            let h = coinS[i].Time2 - 24
+                            coinS[i].Time = h
+                            coinS[i].flagged = true
+                        }
                     }
+                    console.log(coinS)
                    //всички токени който са с днешна дата и са в следващите часове------------
+
                    for(let i =0;i<coinsLenght;i++){
                         if(coinS[i].Date == today){
-                            if(coinS[i].Time > currentTime){
-                                if(coinS[i].DateFormated.length > 20){
-                                   //2021-11-22T22:14:00.000Z
-                                   let indexT = coinS[i].DateFormated.indexOf('T')
-                                   let indexD = coinS[i].DateFormated.indexOf('.')
-                                   
-                                   let newDate = coinS[i].DateFormated.slice(indexT+1,indexD-3)
-                                   coinS[i].DateFormated = newDate
+                            if(coinS[i].Time2 > currentTime){
+                                //PM -----------------------------------
+                                
+                                if(coinS[i].Time2 >12){
+                                    let helpr = coinS[i].Time2 - 12.0
+                                    if(helpr < 10){
+                                        helpr = "0" + helpr.toFixed(2)
+                                        helpr = helpr.replace('.',':')
+                                        helpr = helpr + ' ' + "PM"
+                                        coinS[i].DateFormated = helpr
+                                    }else {
+                                        helpr = helpr.toFixed(2);
+                                        helpr = helpr.replace('.',':')
+                                        helpr = helpr + ' ' + "PM"
+                                        coinS[i].DateFormated = helpr
+                                    }
                                 }
-                                if(coinS[i].Poo != null){
-                                    coinS[i].Poo2 = coinS[i].Poo.slice(0,32)
+                                //AM -----------------------------------
+                                else if(coinS[i].Time2 <= 12){
+                                    let helpr = coinS[i].Time2
+                                    if(helpr ==2){
+                                        coinS[i].DateFormated = "12:00 AM"
+                                    }else if(helpr ==12){
+                                        coinS[i].DateFormated = "12:00 PM"
+                                    }
+                                    else if(helpr < 10){
+                                        helpr = "0" + helpr.toFixed(2)
+                                        helpr = helpr.replace('.',':')
+                                        helpr = helpr + ' ' + "AM"
+                                        coinS[i].DateFormated = helpr
+                                    }else{
+                                        helpr = helpr.toFixed(2);
+                                        helpr = helpr.replace('.',':')
+                                        helpr = helpr + ' ' + "AM"
+                                        coinS[i].DateFormated = helpr
+                                    }
                                 }
-                                if(coinS[i].Pink != null){
-                                    coinS[i].Pink2 = coinS[i].Pink.slice(0,30)
-                                }
-                               
+                                //--------------------------------------
+                                
+                            }
+                            
+                            if(coinS[i].Time2 > currentTime){
+                                
                                 filteredCoins.push(coinS[i])
                             }
                         }
                         
+                        /// TOMORROW -------------------------------------------------------
+                        //----------------------------------------------
+                        //------------------------------------------------------------------
                         if(coinS[i].Date == tomorrowCheck){
                             if(coinS[i].DateFormated.length > 20){
-                                //2021-11-22T22:14:00.000Z
-                                let indexT = coinS[i].DateFormated.indexOf('T')
-                                let indexD = coinS[i].DateFormated.indexOf('.')
+
+                                //PM -------------------------------------------
+                                if(coinS[i].flagged === true){
+                                    console.log("flagged",coinS[i])
+                                    console.log(coinS[i].Time2)
+                                    let helper = coinS[i].Time
+                                    helper = helper.toFixed(2)
+                                    let time2 = -coinS[i].Time
+                                    console.log(time2)
+                                    coinS[i].Time = time2
+                                    tmrHelper.push(coinS[i])
+                                    if(helper == 0){
+                                        coinS[i].DateFormated = "12:00 AM"
+                                    }
+                                    else if(helper < 10){
+                                        helper = "0" + helper + " AM"
+                                        helper = helper.replace('.',':')
+                                        coinS[i].DateFormated = helper
+                                    }
+                                    console.log("helping",helper)
+                                }
+                                else if(coinS[i].Time2 >12){
+                                    let helpr = coinS[i].Time2 - 12.0
+                                    console.log(coinS[i],helpr)
+                                    if(helpr < 10){
+                                        helpr = "0" + helpr.toFixed(2)
+                                        helpr = helpr.replace('.',':')
+                                        helpr = helpr + ' ' + "PM"
+                                        coinS[i].DateFormated = helpr
+                                    }else{
+                                        helpr = helpr.toFixed(2);
+                                        helpr = helpr.replace('.',':')
+                                        helpr = helpr + ' ' + "PM"
+                                        coinS[i].DateFormated = helpr
+                                        console.log(coinS[i])
+                                    }
+                                }
                                 
-                                let newDate = coinS[i].DateFormated.slice(indexT+1,indexD-3)
-                                coinS[i].DateFormated = newDate
+                                //AM -----------------------------------
+                                else if(coinS[i].Time2 <= 12){
+                                    let helpr = coinS[i].Time2
+                                    if(helpr ==2){
+                                        coinS[i].DateFormated = "12:00 AM"
+                                    }else if(helpr ==12){
+                                        coinS[i].DateFormated = "12:00 PM"
+                                    }
+                                    else if(helpr < 10){
+                                        helpr = "0" + helpr.toFixed(2)
+                                        helpr = helpr.replace('.',':')
+                                        helpr = helpr + ' ' + "AM"
+                                        coinS[i].DateFormated = helpr
+                                    }else{
+                                        helpr = helpr.toFixed(2);
+                                        helpr = helpr.replace('.',':')
+                                        helpr = helpr + ' ' + "AM"
+                                        coinS[i].DateFormated = helpr
+                                    }
+                                }
                              }
                              if(coinS[i].Poo != null){
                                 coinS[i].Poo2 = coinS[i].Poo.slice(0,32)
@@ -152,20 +246,33 @@ function Coin2({props}){
                             tmrCoins.push(coinS[i])
                         }
                     }
-                    console.log(filteredCoins) 
+                    //console.log(filteredCoins) 
                     //сортираме в възходящ ред ------------------------------------------------
                     filteredCoins.sort((a,b) => {
                         return a.Time - b.Time
                     })
+                    console.log("tmr",tmrHelper)
+                    tmrHelper.sort((a,b) => {
+                        return b.Time - a.Time
+                    })
+                    console.log("tmr",tmrHelper)
                     tmrCoins.sort((a,b) => {
                         return a.Time - b.Time
                     })
-                    //показваме само следващите 5 токена или останалите ако са под 5 ----------
-                    setCoins(filteredCoins)
+
+                    if(tmrHelper.length > 1){
+                       setTomorrowHelper(tmrHelper)
+                    }
+                    console.log(tomorrowHelper)
+                    tmrCoins.splice(0,tomorrowHelper.length,...tomorrowHelper)
+                   // ----------
+                    //setCoins(filteredCoins)
                     if(filteredCoins.length<20){
                         let index = 20 - filteredCoins.length
                         tmrCoins = tmrCoins.slice(0,index)
                         setTomorrowCoins(tmrCoins)
+                        setCoins(filteredCoins)
+                    }else {
                         setCoins(filteredCoins)
                     }
     
@@ -182,7 +289,7 @@ function Coin2({props}){
         }
 
     },[counter])
-
+    console.log(coins,tomorrowCoins)
     if(coins.length>=20){
         return( <div className="coins-wrapper">
         <p className="token-p">Upcoming events</p>
@@ -210,7 +317,6 @@ function Coin2({props}){
     )
     }else if(coins.length <20){
         return( <div className="coins-wrapper">
-            {console.log("a",coins,tomorrowCoins)}
         <p className="token-p">Upcoming events</p>
         <p className="date-p">TODAY {todayDisplay}</p>
         {coins.map(coin =>  
